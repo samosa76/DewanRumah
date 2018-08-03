042d482e077ad31182bb01417d315c54cb76ff85
sam <muhammadusamah76@gmail.com>
2018-08-03 11:37:37 +0700

    add CardView dependensis

--- app/src/main/java/com/samosauus/dewarumah/AfterLogin/ALBonus.java
+++ app/src/main/java/com/samosauus/dewarumah/AfterLogin/ALBonus.java
@@ -1,11 +1,16 @@
 package com.samosauus.dewarumah.AfterLogin;
 
+import android.support.design.widget.TabLayout;
+import android.support.v4.view.ViewPager;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 
 import com.samosauus.dewarumah.R;
 
 public class ALBonus extends AppCompatActivity {
+
+    TabLayout tabs;
+    ViewPager vpager;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
